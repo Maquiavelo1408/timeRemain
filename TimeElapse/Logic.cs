98946201bd7ac64e180c7797e8d8074f50ffcf98
Rafael Raygoza <rraygo@celestica.com>
2019-02-21 16:09:41 -0600

    first

---
+++ TimeElapse/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeElapse
+{
+    public class Logic
+    {
+        public Logic()
+        {
+
+        }
+
+        public TimeSpan CalculateReaminTimeInSeconds()
+        {
+            var now = DateTime.Now;
+            var bye = new DateTime(2019, 05, 31, 12, 00, 00);
+            var result = bye-now;
+            return result;
+        }
+    }
+}
