cea85bfbd0db638c12c1d3bd2827d9350f1b0ca3
Rafael Raygoza <rraygo@celestica.com>
2019-02-25 10:07:54 -0600

    Progressbar

--- TimeElapse/Logic.cs
+++ TimeElapse/Logic.cs
@@ -18,5 +18,34 @@ namespace TimeElapse
             var result = bye-now;
             return result;
         }
+        public (long, double) PorcentageRemain()
+        {
+            var dateStart = new DateTime(2019, 02, 21, 12, 0, 0);
+            var dateEnd = new DateTime(2019, 05, 31, 12, 0, 0);
+            var currentDate = DateTime.Now;
+
+            var betweenTicks = dateEnd.Ticks - dateStart.Ticks;
+            var currentTicks = currentDate.Ticks - dateStart.Ticks;
+            var bDays = (dateEnd - dateStart).TotalDays;
+            var cDays = (currentDate - dateStart).TotalDays;
+            var thirdRuleTicks = (currentTicks * 100) / betweenTicks;
+            var tRD = (cDays * 100) / bDays;
+            return (thirdRuleTicks, tRD);
+        }
+
+        public string DrawProgressBar(double actualPercetange)
+        {
+            var stringBuilder = new StringBuilder();
+            var actualFloor = Math.Floor(actualPercetange);
+            for(int i = 0; i<actualFloor; i++)
+            {
+                stringBuilder.Append("#");
+            }
+            for(int i = 0; i<100-actualFloor; i++)
+            {
+                stringBuilder.Append("-");
+            }
+            return stringBuilder.ToString();
+        }
     }
 }
--- TimeElapse/Program.cs
+++ TimeElapse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Threading;
 
 namespace TimeElapse
@@ -8,15 +9,19 @@ namespace TimeElapse
         static void Main(string[] args)
         {
             var logic = new Logic();
+            
             while (1 < 2)
             {
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain().Item1));
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain().Item2));
+                Console.WriteLine(logic.DrawProgressBar(logic.PorcentageRemain().Item2));
                 var time = logic.CalculateReaminTimeInSeconds();
-                Console.WriteLine(string.Format("Ticks: {0}", time.Ticks));
-                Console.WriteLine(string.Format("Milliseconds: {0}", time.TotalMilliseconds));
-                Console.WriteLine(string.Format("Seconds: {0}", time.TotalSeconds));
-                Console.WriteLine(string.Format("Minutes: {0}", time.TotalMinutes));
-                Console.WriteLine(string.Format("Hours: {0}", time.TotalHours));
-                Console.WriteLine(string.Format("Days: {0}", time.TotalDays));
+                Console.WriteLine(new StringBuilder().Append(string.Format("Ticks: {0}", time.Ticks)).AppendLine()
+                    .Append(string.Format("Milliseconds: {0}", time.TotalMilliseconds)).AppendLine()
+                    .Append(string.Format("Seconds: {0}", time.TotalSeconds)).AppendLine()
+                    .Append(string.Format("Minutes: {0}", time.TotalMinutes)).AppendLine()
+                    .Append(string.Format("Hours: {0}", time.TotalHours)).AppendLine()
+                    .Append(string.Format("Days: {0}", time.TotalDays)).AppendLine().ToString());
                 //Thread.Sleep(100);
                 Console.ReadLine();
                 Console.Clear();
