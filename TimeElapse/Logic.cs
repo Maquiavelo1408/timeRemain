501b19ee39abf7b6e1f79d526442234d385a7c60
Rafael Raygoza <rraygo@celestica.com>
2019-02-25 15:59:58 -0600

    Added overload methods

--- TimeElapse/Logic.cs
+++ TimeElapse/Logic.cs
@@ -18,10 +18,32 @@ namespace TimeElapse
             var result = bye-now;
             return result;
         }
+        public TimeSpan CalculateReaminTimeInSeconds(int year, int month, int day)
+        {
+            var now = DateTime.Now;
+            var bye = new DateTime(year, month,day, 12, 00, 00);
+            var result = bye - now;
+            return result;
+        }
         public (long, double) PorcentageRemain()
         {
             var dateStart = new DateTime(2019, 02, 21, 12, 0, 0);
             var dateEnd = new DateTime(2019, 05, 31, 12, 0, 0);
+            var currentDate = DateTime.Now;
+
+            var betweenTicks = dateEnd.Ticks - dateStart.Ticks;
+            var currentTicks = currentDate.Ticks - dateStart.Ticks;
+            var bDays = (dateEnd - dateStart).TotalDays;
+            var cDays = (currentDate - dateStart).TotalDays;
+            var thirdRuleTicks = (currentTicks * 100) / betweenTicks;
+            var tRD = (cDays * 100) / bDays;
+            return (thirdRuleTicks, tRD);
+        }
+
+        public (long, double) PorcentageRemain(int year, int month, int day)
+        {
+            var dateStart = new DateTime(2019, 02, 21, 12, 0, 0);
+            var dateEnd = new DateTime(year, month, day, 12, 0, 0);
             var currentDate = DateTime.Now;
 
             var betweenTicks = dateEnd.Ticks - dateStart.Ticks;
--- TimeElapse/Program.cs
+++ TimeElapse/Program.cs
@@ -12,10 +12,48 @@ namespace TimeElapse
             
             while (1 < 2)
             {
-                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain().Item1));
-                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain().Item2));
-                Console.WriteLine(logic.DrawProgressBar(logic.PorcentageRemain().Item2));
-                var time = logic.CalculateReaminTimeInSeconds();
+                Console.WriteLine("Fecha Inicio: 21/02/2019");
+                Console.WriteLine("15/03/2019");
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 03, 15).Item1));
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 03, 15).Item2));
+                Console.WriteLine(logic.DrawProgressBar(logic.PorcentageRemain(2019, 03, 15).Item2));
+                var time1 = logic.CalculateReaminTimeInSeconds(2019, 03, 15);
+                Console.WriteLine(new StringBuilder().Append(string.Format("Ticks: {0}", time1.Ticks)).AppendLine()
+                    .Append(string.Format("Milliseconds: {0}", time1.TotalMilliseconds)).AppendLine()
+                    .Append(string.Format("Seconds: {0}", time1.TotalSeconds)).AppendLine()
+                    .Append(string.Format("Minutes: {0}", time1.TotalMinutes)).AppendLine()
+                    .Append(string.Format("Hours: {0}", time1.TotalHours)).AppendLine()
+                    .Append(string.Format("Days: {0}", time1.TotalDays)).AppendLine().ToString());
+                Console.WriteLine("31/03/2019");
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 03, 31).Item1));
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 03, 31).Item2));
+                Console.WriteLine(logic.DrawProgressBar(logic.PorcentageRemain(2019, 03, 31).Item2));
+                var time3 = logic.CalculateReaminTimeInSeconds(2019, 03, 31);
+                Console.WriteLine(new StringBuilder().Append(string.Format("Ticks: {0}", time3.Ticks)).AppendLine()
+                    .Append(string.Format("Milliseconds: {0}", time3.TotalMilliseconds)).AppendLine()
+                    .Append(string.Format("Seconds: {0}", time3.TotalSeconds)).AppendLine()
+                    .Append(string.Format("Minutes: {0}", time3.TotalMinutes)).AppendLine()
+                    .Append(string.Format("Hours: {0}", time3.TotalHours)).AppendLine()
+                    .Append(string.Format("Days: {0}", time3.TotalDays)).AppendLine().ToString());
+
+                Console.WriteLine("30/04/2019");
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 04, 30).Item1));
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 04, 30).Item2));
+                Console.WriteLine(logic.DrawProgressBar(logic.PorcentageRemain(2019, 04, 30).Item2));
+                var time2 = logic.CalculateReaminTimeInSeconds(2019, 04, 30);
+                Console.WriteLine(new StringBuilder().Append(string.Format("Ticks: {0}", time2.Ticks)).AppendLine()
+                    .Append(string.Format("Milliseconds: {0}", time2.TotalMilliseconds)).AppendLine()
+                    .Append(string.Format("Seconds: {0}", time2.TotalSeconds)).AppendLine()
+                    .Append(string.Format("Minutes: {0}", time2.TotalMinutes)).AppendLine()
+                    .Append(string.Format("Hours: {0}", time2.TotalHours)).AppendLine()
+                    .Append(string.Format("Days: {0}", time2.TotalDays)).AppendLine().ToString());
+
+
+                Console.WriteLine("31/05/2019");
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 05, 31).Item1));
+                Console.WriteLine(string.Format("{0}%", logic.PorcentageRemain(2019, 05, 31).Item2));
+                Console.WriteLine(logic.DrawProgressBar(logic.PorcentageRemain(2019, 05, 31).Item2));
+                var time = logic.CalculateReaminTimeInSeconds(2019, 05, 31);
                 Console.WriteLine(new StringBuilder().Append(string.Format("Ticks: {0}", time.Ticks)).AppendLine()
                     .Append(string.Format("Milliseconds: {0}", time.TotalMilliseconds)).AppendLine()
                     .Append(string.Format("Seconds: {0}", time.TotalSeconds)).AppendLine()
